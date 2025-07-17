43fae61984499421a5649f4a9513465bcb354f17
Yahya <yahyabutt2000@gmail.com>
2025-07-17 14:39:31 +0500

    Created sql connector and short code generator functions

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import string
 import random
 from datetime import datetime
 from config import MYSQL_CONFIG
+
+app = Flask(__name__)
+
+def get_db():
+    return mysql.connector.connect(**MYSQL_CONFIG)
+
+def generate_short_code(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
